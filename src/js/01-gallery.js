de90a35dd6fd2cf6888da089b5e44643168b6f9d
LiliiaBarsuk <lilya.b@live.com>
2022-08-07 22:15:25 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryElement = createGalleryItems(galleryItems);
+
+function createGalleryItems(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+         <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+         </a>
+         </li>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryElement);
+var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playbackTime = function(currentSecond) {
+        let time = 0;
+        time = currentSecond.seconds
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(time))
+       console.log(time);
+    }
+
+
+    player.on('timeupdate', throttle(playbackTime, 1000));
+
+    const currentTime = localStorage.getItem("videoplayer-current-time")
+    player.setCurrentTime(JSON.parse(currentTime))
 
