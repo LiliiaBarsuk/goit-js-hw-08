320a1011a7effd520db38259476256216065f4f2
LiliiaBarsuk <lilya.b@live.com>
2022-08-09 16:26:26 +0300

    start task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,12 +7,11 @@ const playbackTime = function(currentSecond) {
         let time = 0;
         time = currentSecond.seconds
         localStorage.setItem("videoplayer-current-time", JSON.stringify(time))
-       console.log(time);
     }
-
 
     player.on('timeupdate', throttle(playbackTime, 1000));
 
     const currentTime = localStorage.getItem("videoplayer-current-time")
+    
     player.setCurrentTime(JSON.parse(currentTime))
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle";
 
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    email: document.querySelector("input"),
+    message: document.querySelector("textarea"),
+    btn: document.querySelector("button")
+}
+
+autocompleteOnLoad(); 
+
+refs.form.addEventListener('input', throttle(saveData, 500))
+refs.form.addEventListener('submit', onFormSubmit)
+
+let obj = {}
+
+function saveData(event) {
+   obj[event.target.name] = event.target.value;
+
+   localStorage.setItem("feedback-form-state", JSON.stringify(obj))
+}
+
+function onFormSubmit(evt) {
+   evt.preventDefault();
+   evt.currentTarget.reset();
+   console.log(JSON.parse(localStorage.getItem("feedback-form-state")))
+   localStorage.removeItem("feedback-form-state")
+}
+
+function autocompleteOnLoad() {
+    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"))
+    
+    if (savedData) {
+      refs.email.value = savedData.email;
+      refs.message.value = savedData.message;
+    }
+    
+  
+}
